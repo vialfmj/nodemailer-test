4a0740070f653622d4425433ac7823f18fa1c591
vialfmj <vialfmj@gmail.com>
2022-08-15 01:18:19 -0300

    template string correction

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 const {setUpMail, sendEmail} = require("./sendMail")
 
 const destinator = "vialfmj@gmail.com"
-const html = '<h1 style="color: blue;">Mail de prueba</h1>'
+const html = `<h1 style="color: blue;">Mail de prueba</h1>`
 const attachments = [
     {
         path: 'src/descarga.png'
